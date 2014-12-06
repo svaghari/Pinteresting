3471393251e8ab56ed8eb7bfc0e200355015bc89
Sohail Vaghari <sohailvaghari@gmail.com>
2014-12-05 21:53:37 -0800

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,3 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+#required for Heroku
+
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
